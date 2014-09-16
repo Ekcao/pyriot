f4c06b4228218673d099dda5f3dc874a6c7675fe
Eric Cao <eric0cao@gmail.com>
2014-09-15 22:19:11 -0700

    Clean up file r/w

--- pyriot/pyriot.py
+++ pyriot/pyriot.py
@@ -5,19 +5,20 @@ import riotlol
 
 def main():
     file_name = 'info.json'
+
     if os.path.isfile(file_name):
         f = open(file_name, 'r')
-        user_data = json.load(f)
-        print(user_data['api_key'])
+        info = json.load(f)
     else:
         f = open(file_name, 'w')
         api_key = input("Enter Riot Games API key: ")
-        riot = riotlol.RiotLOL(api_key)
+
         info = {'api_key': api_key,
                 'version': riot.latest_version()
                 }
         json.dump(info, f)
 
+    riot = riotlol.RiotLOL(info['api_key'])
     f.close()
 
 if __name__ == '__main__':
