c7ed258c722ba02760db1ee03b9b7e86ff9acbb9
Eric Cao <eric0cao@gmail.com>
2014-10-23 00:20:52 -0700

    Moved to spell class: coefficient, eval_tooltip, and __str__

--- pyriot/pyriot.py
+++ pyriot/pyriot.py
@@ -2,6 +2,7 @@ import os.path
 import json
 import argparse
 import riotlol
+import spell
 
 
 def parse():
@@ -16,74 +17,68 @@ def parse():
     return parser.parse_args()
 
 
-def print_spell(spell):
-    fmt = '\n{0}\nRange: {1}  Cost: {2}  Cooldown: {3}'
-    description = fmt.format(
-        spell['name'],
-        spell['rangeBurn'].capitalize(),
-        spell['resource'].replace(
-            '{{ cost }}', spell['costBurn']
-        ),
-        spell['cooldownBurn']
-    )
-    print(description)
-    print(spell_tooltip(spell))
-
-
-def coefficient(spell):
-    coeffs = {}
-
-    damage_type = {
-        'spelldamage': ' AP',
-        'bonusattackdamage': ' bonus AD',
-        'attackdamage': ' AD'
-    }
-
-    try:
-        for i in range(len(spell['vars'])):
-            coeff = spell['vars'][i]['link']
-            k = spell['vars'][i]['key']
-            coeff_type = damage_type[coeff]
-            coeffs[k] = str(spell['vars'][i]['coeff'][0]) + coeff_type
-    except KeyError:
-        pass
-
-    return coeffs
-
-
-def spell_tooltip(spell):
-    tooltip = spell['sanitizedTooltip']
-
-    damage_type = {
-        'spelldamage': ' AP',
-        'bonusattackdamage': ' bonus AD',
-        'attackdamage': ' AD'
-    }
-
-    # Replace ei with effect burns (e.g. 50/60/70/80/90)
-    for i in range(1, len(spell['effectBurn'])):
-        tooltip = tooltip.replace(
-            '{{ e' + str(i) + ' }}',
-            spell['effectBurn'][i]
-        )
-
-    coeffs = coefficient(spell)
-
-    if not coeffs:
-        return tooltip
-
-    try:
-        for i in range(len(spell['vars'])):
-            k = spell['vars'][i]['key']
-
-            tooltip = tooltip.replace(
-                '{{ ' + k + ' }}',
-                coeffs[k]
-            )
-    except KeyError:
-        pass
-
-    return tooltip
+# def print_spell(spell):
+#     fmt = '\n{0}\nRange: {1}  Cost: {2}  Cooldown: {3}'
+#     description = fmt.format(
+#         spell['name'],
+#         spell['rangeBurn'].capitalize(),
+#         spell['resource'].replace(
+#             '{{ cost }}', spell['costBurn']
+#         ),
+#         spell['cooldownBurn']
+#     )
+#     print(description)
+#     print(spell_tooltip(spell))
+#
+#
+# def coefficient(spell):
+#     coeffs = {}
+#
+#     damage_type = {
+#         'spelldamage': ' AP',
+#         'bonusattackdamage': ' bonus AD',
+#         'attackdamage': ' AD'
+#     }
+#
+#     try:
+#         for i in range(len(spell['vars'])):
+#             coeff = spell['vars'][i]['link']
+#             k = spell['vars'][i]['key']
+#             coeff_type = damage_type[coeff]
+#             coeffs[k] = str(spell['vars'][i]['coeff'][0]) + coeff_type
+#     except KeyError:
+#         pass
+#
+#     return coeffs
+#
+#
+# def spell_tooltip(spell):
+#     tooltip = spell['sanitizedTooltip']
+#
+# Replace ei with effect burns (e.g. 50/60/70/80/90)
+#     for i in range(1, len(spell['effectBurn'])):
+#         tooltip = tooltip.replace(
+#             '{{ e' + str(i) + ' }}',
+#             spell['effectBurn'][i]
+#         )
+#
+#     coeffs = coefficient(spell)
+#
+#     if not coeffs:
+#         return tooltip
+#
+#     try:
+#         for i in range(len(spell['vars'])):
+#             k = spell['vars'][i]['key']
+#
+#             tooltip = tooltip.replace(
+#                 '{{ ' + k + ' }}',
+#                 coeffs[k]
+#             )
+#     except KeyError:
+#         pass
+#
+#     return tooltip
 
 
 def main():
@@ -107,10 +102,10 @@ def main():
 
         ability = {
             'p': ch['passive'],
-            'q': ch['spells'][0],
-            'w': ch['spells'][1],
-            'e': ch['spells'][2],
-            'r': ch['spells'][3]
+            'q': spell.Spell(ch['spells'][0]),
+            'w': spell.Spell(ch['spells'][1]),
+            'e': spell.Spell(ch['spells'][2]),
+            'r': spell.Spell(ch['spells'][3])
         }
 
         for i in args.spell:
@@ -120,7 +115,7 @@ def main():
                     ability[i]['sanitizedDescription']
                 ))
             else:
-                print_spell(ability[i])
+                print(ability[i])
 
             print('\n_______________________________________________________')
     else:
